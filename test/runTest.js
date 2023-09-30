f22e24e4a5cdcb40bd1cb420ed4fc000c7a67234
Gabriel Rose <gabriel.rose@get10up.com>
2023-09-29 23:46:54 -0400

    Test initial install

--- test/runTest.js
+++ test/runTest.js
@@ -13,7 +13,7 @@ async function main() {
 		const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
 		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath, vscodeExecutablePath: '/usr/bin/code'});
+		await runTests({ extensionDevelopmentPath, extensionTestsPath});
 	} catch (err) {
 		console.error('Failed to run tests', err);
 		process.exit(1);
