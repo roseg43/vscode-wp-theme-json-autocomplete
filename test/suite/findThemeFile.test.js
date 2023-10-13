a33f56362a5c73cbcc7be852680777ceb1b59475
Gabriel Rose <gabriel.rose@get10up.com>
2023-10-13 13:53:09 -0400

    findThemeFile: properly stub API in tests, fix typo in extension configuration key

--- test/suite/findThemeFile.test.js
+++ test/suite/findThemeFile.test.js
@@ -6,11 +6,16 @@ const vscode = require('vscode');
 suite('theme.json file locator', () => {
     test('Uses the user-defined path if one is set in extension settings', async () => {
         
-        // Mock the vscode.workspace.getConfiguration method to return the user-defined path.
-        sinon.stub(vscode.workspace, 'getConfiguration').returns({
+        // Stub vscode.workspace.getConfiguration method to return the user-defined path.
+        const workspaceGetStub = sinon.stub();
+        workspaceGetStub.withArgs('themeJsonPath').returns('/path/to/theme.json');
+
+        const workspaceStub = sinon.stub(vscode.workspace, 'getConfiguration');
+        workspaceStub.withArgs('wordpressThemeJsonCssAutosuggest').returns({
             ...vscode.workspace.getConfiguration.prototype,
-            get: () => '/path/to/theme.json',
+            get: workspaceGetStub,
         });
+
         const result = await findThemeFile();
 
         assert.strictEqual(result, '/path/to/theme.json');
